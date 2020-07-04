a5c8ba390dfabf61427d9ab6371e5b2a17997f86
Munkh-Od <eikyuu.hoshi@gmail.com>
2020-07-04 20:33:43 +0800

    Added Accessory

---
+++ database/factories/AccessoryFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Accessory;
+use Faker\Generator as Faker;
+
+$factory->define(Accessory::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_07_04_120459_create_accessories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccessoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accessories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id');
+            $table->integer("unit");
+            $table->text("measurements");
+            $table->text("general_info_1");
+            $table->text("general_info_2");
+            $table->text("general_info_3");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accessories');
+    }
+}
