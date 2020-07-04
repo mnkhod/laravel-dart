89e87e75b32a2dc2944346bc2803c6c34c96ba9e
Munkh-Od <eikyuu.hoshi@gmail.com>
2020-07-04 20:45:12 +0800

    Added Soft Tip Table

---
+++ database/factories/SoftTipFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\SoftTip;
+use Faker\Generator as Faker;
+
+$factory->define(SoftTip::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
---
+++ database/migrations/2020_07_04_124148_create_soft_tips_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSoftTipsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('soft_tips', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger("barrel_weight");
+            $table->bigInteger("full_weight");
+            $table->bigInteger("weight");
+            $table->bigInteger("dart_weight");
+            $table->text("dart_tungsten");
+            $table->text("dart_set");
+            $table->text("dart_flight");
+            $table->text("dart_shaft");
+            $table->text("dimensions");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('soft_tips');
+    }
+}
